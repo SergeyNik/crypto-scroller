49ee5f8951f492a9e623791b75739b30de687ba9
snikolchikov <nikolchikov@gmail.com>
2025-05-15 19:47:10 +0300

    init

---
+++ src/main/java/com/github/sergeynik/cryptoscroller/CryptoScrollerApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergeynik.cryptoscroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoScrollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoScrollerApplication.class, args);
+    }
+
+}
